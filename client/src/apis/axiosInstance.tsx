ba8d5956de049327b46e0e44f955ba4de1cc68ea
fauzankvr <muhammedfauzan7862@gmail.com>
2025-07-18 22:26:40 +0530

    fix: bug in api

--- client/src/apis/axiosInstance.tsx
+++ client/src/apis/axiosInstance.tsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+
+console.log(`  Creating Axios instance with base URL: ${import.meta.env.VITE_API_URL}`);
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
